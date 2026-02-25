d2727401408696338b3604cba6e7b4a85d682644
Sölvi Sigmarsson <solvi24@ru.is>
2026-02-25 12:48:09 +0000

    Tasks and notes finished

--- app.js
+++ app.js
@@ -9,14 +9,25 @@ const BASE_URL = "http://localhost:3000/api/v1";
 /* =========================
    TASK FEATURE
 ========================= */
-
+// Gets task with a get request.
 const getTasks = async () => {
+  // Try to get the tasks if it fails we catch the error.
   try {
     const response = await axios.get(`${BASE_URL}/tasks`);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log("Error getting tasks", error);
+  }
+};
+
+const loadTasks = async () => {
+  try {
+    // gets the tasks 
+    const tasks = await getTasks();
+    // Renders the tasks to display
+    renderTasks(tasks);
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -25,15 +36,21 @@ const renderTasks = (tasks) => {
   taskList.innerHTML = "";
   if (taskList) {
     taskList.innerHTML = "";
+    // Go through each task in the list 
     tasks.forEach((task) => {
+      // Create a li element so we get a lsit of tasks
       const li = document.createElement("li");
 
+      // Create a checkbox input for each task
       const checkbox = document.createElement("input");
       checkbox.type = "checkbox";
       checkbox.checked = task.finished === 1;
 
-      li.textContent = task.task;
-
+      // Task text content created here
+      const label = document.createElement("span");
+      label.textContent = task.task;
+      
+      // If the checkbox is clicked the task status in the backend is updated. 0 if not checked, 1 if checked
       checkbox.addEventListener("change", async () => {
         const taskStatus = checkbox.checked ? 1 : 0;
         try {
@@ -44,6 +61,7 @@ const renderTasks = (tasks) => {
       });
 
       li.append(checkbox);
+      li.append(label);
       taskList.append(li);
     });
   }
@@ -55,8 +73,101 @@ const changeTaskStatus = async (taskId, tasksStatus) => {
   });
 };
 
-const createTask = (async) => {
-  
+const createTask = async (taskText) => {
+  return await axios.post(`${BASE_URL}/tasks`, {
+    task: taskText,
+  });
+};
+
+const handleCreateTask = async () => {
+  const input = document.getElementById("new-task");
+  const taskText = input.value;
+
+  // If there is no text we can't create a new task
+  if (!taskText) return;
+
+  try {
+    await createTask(taskText);
+    input.value = "";
+    await loadTasks();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const wireTaskEvents = () => {
+  const button = document.getElementById("add-task-btn");
+  const input = document.getElementById("new-task");
+  if (button) {
+    button.addEventListener("click", handleCreateTask);
+  }
+
+  if (input) {
+    input.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleCreateTask();
+      }
+    });
+  }
+};
+
+/* =========================
+   NOTE FEATURE
+========================= */
+const getNotes = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/notes`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log("Error getting notes", error);
+    return "";
+  }
+};
+
+const renderNotes = (notes) => {
+  const textArea = document.getElementById("notes-text");
+  if (textArea) {
+    textArea.value = notes;
+  }
+};
+
+const loadNotes = async () => {
+  try {
+    const notes = await getNotes();
+    renderNotes(notes.notes);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const saveNotes = async (text) => {
+  try {
+    const button = document.getElementById("save-notes-btn");
+    if (button) button.disabled = true;
+    const response = await axios.put(`${BASE_URL}/notes`, {
+      notes: text,
+    });
+    await loadNotes();
+    return response.data
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const wireNotesEvent = () => {
+  const textArea = document.getElementById("notes-text");
+  const button = document.getElementById("save-notes-btn");
+  // When the text is modified the button becomes eabled.
+  textArea.addEventListener("input", () => {
+    button.disabled = false;
+  });
+  // When the save button is clicked the modified notes are updated in the backend
+  button.addEventListener("click", () => {
+    const text = textArea.value;
+    saveNotes(text);
+  });
 };
 
 /* =========================
@@ -120,14 +231,14 @@ const wireQuoteEvents = () => {
  */
 const init = async () => {
   wireQuoteEvents();
+  wireTaskEvents();
+  wireNotesEvent();
 
   const select = document.getElementById("quote-category-select");
   const category = select?.value || "general";
-  const tasks = await getTasks();
-
   await loadQuote(category);
-  await getTasks();
-  renderTasks(tasks);
+  await loadTasks();
+  await loadNotes();
 };
 
 /* =========================
